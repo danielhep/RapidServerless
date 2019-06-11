ecaf2fd290f34a93e3241312ba2ffaf53ad888e6
Daniel Heppner <daniel.heppner1@gmail.com>
2019-06-11 01:11:30 -0700

    several working functions

---
+++ handler.js
@@ -0,0 +1,107 @@
+'use strict'
+const mongoose = require('mongoose')
+const { DateTime } = require('luxon')
+
+const dbLookup = require('./src/dbLookups.js')
+
+const Stops = require('gtfs/models/gtfs/stop')
+const Routes = require('gtfs/models/gtfs/route')
+const Trips = require('gtfs/models/gtfs/trip')
+const StopTimes = require('gtfs/models/gtfs/stop-time')
+const Calendars = require('gtfs/models/gtfs/calendar')
+const CalendarDates = require('gtfs/models/gtfs/calendar-date')
+
+const agencyKey = 'whatcom-transit-authority'
+
+const mongoURI = 'mongodb+srv://readonly:1sv5GULN0Fp5WXd5@cluster0-dguyv.azure.mongodb.net/gtfsdump?retryWrites=true&w=majority'
+
+let conn = null
+
+dbLookup.configure({ agencyKey })
+
+let connectToDatabase = async function (context) {
+  context.callbackWaitsForEmptyEventLoop = false
+  console.log('Creating connection.')
+  if (conn == null) {
+    conn = await mongoose.createConnection(mongoURI, {
+      // Buffering means mongoose will queue up operations if it gets
+      // disconnected from MongoDB and send them when it reconnects.
+      // With serverless, better to fail fast if not connected.
+      bufferCommands: false, // Disable mongoose buffering
+      bufferMaxEntries: 0, // and MongoDB driver buffering
+      connectTimeoutMS: 1000
+    })
+  } else {
+    console.log('Already connected!')
+  }
+  conn.model('Stops', Stops.schema)
+  conn.model('Routes', Routes.schema)
+  conn.model('Trips', Trips.schema)
+  conn.model('StopTimes', StopTimes.schema)
+  conn.model('Calendars', Calendars.schema)
+  conn.model('CalendarDates', CalendarDates.schema)
+  console.log('Connected')
+}
+
+module.exports.getStops = async (event, context) => {
+  await connectToDatabase(context)
+
+  if (conn) console.log('Definitely connected, getting stops')
+
+  let stops = await conn.model('Stops').find(
+    { agency_key: agencyKey }
+  ).exec()
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify(stops, null, 2)
+  }
+}
+
+module.exports.getRoutes = async (event, context) => {
+  await connectToDatabase(context)
+
+  if (conn) console.log('Definitely connected, getting routes')
+
+  let routes = await conn.model('Routes').find(
+    { agency_key: agencyKey }
+  ).exec()
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify(routes, null, 2)
+  }
+}
+
+module.exports.getScheduleByStop = async (event, context) => {
+  await connectToDatabase(context)
+
+  dbLookup.setConnection(conn)
+
+  let routeNames = event.multiValueQueryStringParameters.route
+  let stopId = event.multiValueQueryStringParameters.stop
+
+  let date = DateTime.fromISO(event.multiValueQueryStringParameters.date)
+  let serviceIds = await dbLookup.getServiceCodes(date)
+  // get a list of routes from the route names
+  let routes = await dbLookup.getRoutes(routeNames)
+  let trips = await dbLookup.getTripsFromRouteNames(serviceIds, routes)
+
+  let stopTimes = await dbLookup.getStopTimesFromTrips(stopId, serviceIds, trips)
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({ stopTimes, trips, routes, serviceIds }, null, 2)
+  }
+}
---
+++ src/dbLookups.js
@@ -0,0 +1,139 @@
+const _ = require('lodash')
+const { DateTime, Interval } = require('luxon')
+
+let agencyKey
+
+let Stops
+let Routes
+let Trips
+let StopTimes
+let Calendars
+let CalendarDates
+
+exports.configure = (config) => {
+  agencyKey = config.agencyKey
+}
+
+exports.setConnection = (conn) => {
+  Stops = conn.model('Stops')
+  Routes = conn.model('Routes')
+  Trips = conn.model('Trips')
+  StopTimes = conn.model('StopTimes')
+  Calendars = conn.model('Calendars')
+  CalendarDates = conn.model('CalendarDates')
+}
+
+exports.getCalendars = async () => {
+  let calenders = await Calendars.find({ agency_key: agencyKey }).exec()
+  return calenders
+}
+
+exports.getCalendarDates = async () => {
+  let dates = await CalendarDates.find({ agency_key: agencyKey }).exec()
+  return dates
+}
+
+exports.getServiceCodes = async (searchDate) => {
+  let dayName = searchDate.toFormat('EEEE').toLowerCase()
+  let calendars = await exports.getCalendars()
+  let serviceIds = []
+  calendars.forEach(item => {
+    // also ensure that today is within the valid date range
+    let startDate = DateTime.fromFormat(item.start_date.toString(), 'yyyyLLdd')
+    let endDate = DateTime.fromFormat(item.end_date.toString(), 'yyyyLLdd')
+    let interval = Interval.fromDateTimes(startDate, endDate)
+
+    if (item[dayName] && interval.contains(searchDate)) {
+      serviceIds.push(item.service_id)
+    }
+    // TODO: Indicate if there is no calendar dates
+  })
+
+  // add service IDs for calendar-dates
+  let calendarDates = await exports.getCalendarDates()
+  calendarDates.forEach(item => {
+    let date = DateTime.fromFormat(item.date.toString(), 'yyyyLLdd')
+    if (+date === +searchDate) {
+      if (item.exception_type === 1) { // INCLUDE service on this date
+        serviceIds.push(item.service_id)
+      } else if (item.exception_type === 2) { // EXCLUDE service from this date
+        // log(item.service_id)
+        _.pull(serviceIds, item.service_id)
+      }
+    }
+  })
+
+  return serviceIds
+}
+
+exports.getAllStops = async () => {
+  let stops = await Stops.find(
+    { agency_key: agencyKey }
+  ).exec()
+
+  return stops
+}
+
+exports.getStopIdFromStopCode = async (stopCode) => {
+  // stopCode = toString(stopCode)
+  let stops = await Stops.find(
+    { stop_code: stopCode, agency_key: agencyKey },
+    'stop_id'
+  ).exec()
+  let stopId = stops[0].stop_id
+  return stopId
+}
+
+exports.getRoutes = async (routeNames) => {
+  let query = routeNames.length
+    ? { route_short_name: { $in: routeNames }, agency_key: agencyKey }
+    : { agency_key: agencyKey }
+  let routes = await Routes.find(query, 'route_id route_short_name').exec()
+  return routes
+}
+
+exports.getTripsFromRouteNames = async (serviceIds, routes) => {
+  let trips
+  if (routes) { // only filter by routenames if the caller passed in routenames
+    let routeIds = _.map(routes, 'route_id')
+    trips = await Trips.find(
+      {
+        route_id: { $in: routeIds },
+        service_id: { $in: serviceIds },
+        agency_key: agencyKey
+      },
+      'trip_id route_id service_id trip_headsign'
+    ).exec()
+  } else {
+    trips = await Trips.find({
+      service_id: { $in: serviceIds },
+      agency_key: agencyKey
+    }, 'trip_id route_id service_id trip_headsign'
+    ).exec()
+  }
+
+  return trips
+}
+
+exports.getStopTimesFromTrips = async (stopId, serviceIds, trips) => {
+  let query = {
+    stop_id: stopId,
+    // pickup_type: 0, // for some reason the new WTA times don't have pickup types
+    agency_key: agencyKey
+  }
+
+  if (trips) {
+    query['trip_id'] = { $in: _.map(trips, 'trip_id') }
+  }
+
+  console.log(`StopId: ${stopId}`)
+  console.log(`Trip: ${trips[0]}`)
+
+  let stopTimes = await StopTimes.find(query, 'trip_id departure_time').exec()
+  console.log(`stopTime: ${stopTimes}`)
+
+  stopTimes = _.sortBy(stopTimes, s =>
+    DateTime.fromFormat(s.departure_time, 'H:mm:ss')
+  )
+  return stopTimes
+}
